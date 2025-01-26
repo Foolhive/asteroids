f46c442e1631f28d7997e4cc83f11a3de2e85ee7
Foolhive <seanedwardtzn@gmail.com>
2025-01-25 17:10:56 -0800

    asteroids and asteroidfield added, edited constants, player, and main. Now able to move and spawn asteroids.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
+from asteroids import *
+from AsteroidField import *
 
 def main ():
     pygame.init()
@@ -10,16 +12,29 @@ def main ():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
-        player.draw(screen)
+
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:   
+            sprite.draw(screen)  # Use your custom draw method
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
